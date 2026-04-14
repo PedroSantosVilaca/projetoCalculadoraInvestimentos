263bcc16730bf8f440b56efa1dc9f876c2668801
Pedro Vilaça <eu.sou.pedro.vilaca@gmail.com>
2026-04-14 04:19:30 -0300

    comecei a pegar os valores do formulário

--- calculadoraInvestimentos/src/main.js
+++ calculadoraInvestimentos/src/main.js
@@ -1,1 +1,12 @@
 import "./style.css";
+
+const calculateButton = document.getElementById("calculate-results");
+
+function renderProgression() {
+  const startingAmount = document.getElementById("starting-amount").value;
+  const additionalContribution = document.getElementById(
+    "additional-contribuition",
+  ).value;
+  const timeAmount = document.getElementById("time-amount").value;
+  const timeAmountPeriod = document.getElementById("time-amount-period").value;
+}
