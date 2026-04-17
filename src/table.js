050898a9fb5d448d7f71a5f7f9f36306c58e287c
Pedrovilacawjcasino <pedrovilaca@up.bet.br>
2026-04-17 10:17:27 -0300

    adicionei mais coisas

--- src/table.js
+++ src/table.js
@@ -15,10 +15,25 @@ const createTable = (columnsArray, dataArray, tableID) => {
     throw new Error("O ID informado não corresponde a nenhum elemento table");
   }
 
-  createTableHeader();
+  createTableHeader(tableElement, columnsArray);
   createTableBody();
 };
 
-function createTableHeader() {}
+function createTableHeader(tableReference, columnsArray) {
+  function createTheadElement(tableReference) {
+    const thead = document.createElement("thead");
+    tableReference.appendChild(thead);
+    return thead;
+  }
+  const tableHeaderReference =
+    tableReference.querySelector("thead") ?? createTheadElement(tableReference);
+  const headerRow = document.createElement("tr");
+  for (const tableColumnObject of columnsArray) {
+    const headerElement = `<th class='text-center'>${tableColumnObject.columnLabel}</th>`;
+    headerRow.innerHTML += headerElement;
+
+    tableHeaderReference.appendChild(headerRow);
+  }
+}
 
 function createTableBody() {}
